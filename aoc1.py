310f1db1c7041394e9aef96c4fbe0cc95a58b4b6
spoohie <kplatkowski@outlook.com>
2021-12-02 09:54:42 +0100

    Enclose logic in functions

--- aoc1.py
+++ aoc1.py
@@ -1,38 +1,39 @@
 import itertools
 
-
-with open('input.txt', 'r') as f:
-    data = [int(i) for i in f] 
-
-
 def pairwise(iterable):
     a, b = itertools.tee(iterable)
     next(b, None)
-    return zip(a, b)   
+    return zip(a, b)
 
 
-val = 0    
-
-for a, b in pairwise(data):
-    if a < b:
-        val = val + 1
-
-print(f"Part one solution: {val}")
-
-
-def threes(iterator):
+def threes(iterable):
     "s -> (s0,s1,s2), (s1,s2,s3), (s2, s3,4), ..."
-    a, b, c = itertools.tee(iterator, 3)
+    a, b, c = itertools.tee(iterable, 3)
     next(b, None)
     next(c, None)
     next(c, None)
     return zip(a, b, c)
 
+def part_one(data):
+    val = 0    
 
-val = 0
+    for a, b in pairwise(data):
+        if a < b:
+            val = val + 1
+    return val
 
-for a, b in pairwise(threes(data)):
-    if sum(a) < sum(b):
-        val = val + 1
 
-print(f"Part two solution: {val}")
+def part_two(data):
+    val = 0
+
+    for a, b in pairwise(threes(data)):
+        if sum(a) < sum(b):
+            val = val + 1
+    return val
+
+
+with open('input.txt', 'r') as f:
+    data = [int(i) for i in f] 
+
+print(f"Part one solution: {part_one(data)}")
+print(f"Part two solution: {part_two(data)}")
--- aoc2.py
+++ aoc2.py
@@ -4,37 +4,41 @@ from collections import namedtuple
 Move = namedtuple('Move', 'dir val')
 
 
+def part_one(moves):
+    hor = 0
+    depth = 0
+
+    for m in moves:
+        if m.dir == "forward":
+            hor = hor + m.val
+        if m.dir == "down":
+            depth = depth + m.val
+        if m.dir == "up":
+            depth = depth - m.val
+    return hor*depth
+
+
+def part_two(moves):
+    hor = 0
+    depth = 0
+    aim = 0
+
+    for m in moves:
+        if m.dir == "forward":
+            hor = hor + m.val
+            depth = depth + aim * m.val
+        if m.dir == "down":
+            aim = aim + m.val
+        if m.dir == "up":
+            aim = aim - m.val
+    return hor*depth
+
+
 with open('input.txt', 'r') as f:
-    data = [[i for i in line.rstrip().split(' ')] for line in f]
+    for line in f:
+        data = [[i for i in line.rstrip().split(' ')] for line in f]
 
 moves = [Move(d[0], int(d[1])) for d in data]
 
-
-hor = 0
-depth = 0
-
-for m in moves:
-    if m.dir == "forward":
-        hor = hor + m.val
-    if m.dir == "down":
-        depth = depth + m.val
-    if m.dir == "up":
-        depth = depth - m.val
-
-print(f"Part one solution: {hor*depth}")
-
-
-hor = 0
-depth = 0
-aim = 0
-
-for m in moves:
-    if m.dir == "forward":
-        hor = hor + m.val
-        depth = depth + aim * m.val
-    if m.dir == "down":
-        aim = aim + m.val
-    if m.dir == "up":
-        aim = aim - m.val
-
-print(f"Part two solution: {hor*depth}")
+print(f"Part one solution: {part_one(moves)}")
+print(f"Part two solution: {part_two(moves)}")

f3ddcfe8822427697adfcb577506a364ebef817a
spoohie <kplatkowski@outlook.com>
2021-12-02 02:44:41 +0100

    Refactor of day 2

--- aoc2.py
+++ aoc2.py
@@ -1,30 +1,40 @@
+from collections import namedtuple
+
+
+Move = namedtuple('Move', 'dir val')
+
+
 with open('input.txt', 'r') as f:
     data = [[i for i in line.rstrip().split(' ')] for line in f]
+
+moves = [Move(d[0], int(d[1])) for d in data]
+
 
 hor = 0
 depth = 0
 
-for d in data:
-    if d[0] == "forward":
-        hor = hor + int(d[1])
-    if d[0] == "down":
-        depth = depth + int(d[1])
-    if d[0] == "up":
-        depth = depth - int(d[1])
+for m in moves:
+    if m.dir == "forward":
+        hor = hor + m.val
+    if m.dir == "down":
+        depth = depth + m.val
+    if m.dir == "up":
+        depth = depth - m.val
 
 print(f"Part one solution: {hor*depth}")
+
 
 hor = 0
 depth = 0
 aim = 0
 
-for d in data:
-    if d[0] == "forward":
-        hor = hor + int(d[1])
-        depth = depth + (aim * int(d[1]))
-    if d[0] == "down":
-        aim = aim + int(d[1])
-    if d[0] == "up":
-        aim = aim - int(d[1])
+for m in moves:
+    if m.dir == "forward":
+        hor = hor + m.val
+        depth = depth + aim * m.val
+    if m.dir == "down":
+        aim = aim + m.val
+    if m.dir == "up":
+        aim = aim - m.val
 
 print(f"Part two solution: {hor*depth}")

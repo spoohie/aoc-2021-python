e533ae02d6ec0c52af10be3b940d466ce81ad89e
spoohie <kplatkowski@outlook.com>
2021-12-10 09:46:46 +0100

    Add solution for day 9

--- aoc9.py
+++ aoc9.py
@@ -1,24 +1,53 @@
-def part_one(data):
-    lowest = list()
-    for y, line in enumerate(data):
-        for x, v in enumerate(line):
-            l=r=u=d=10
-            if x > 0:
-                l = int(line[x-1])
-            if x < len(line)-1:
-                r = int(line[x+1])
-            if y > 0:
-                u = int(data[y-1][x])
-            if y < len(data)-1:
-                d = int(data[y+1][x])
-            v = int(v)
-            if v < l and v < r and v < u and v < d:
-                lowest.append(v+1)
-    return sum(lowest)
+from itertools import product
+
+
+def part_one(depths):
+    lowest = lowest_points(depths)
+    return sum(depths[l]+1 for l in lowest)
+
+
+def lowest_points(depths):
+    lowest_points = list()
+    for node, depth in depths.items():
+        neigbour_depths = [depths[n] for n in find_neighbours(node, depths)]
+        if all(v > depth for v in neigbour_depths):
+            lowest_points.append(node)
+    return lowest_points
+
+
+def find_neighbours(node, depths):
+    return [k for k in [(node[0]-1, node[1]), (node[0]+1, node[1]), (node[0], node[1]-1), (node[0], node[1]+1)] if k in depths]
+
+
+def part_two(depths):
+    lowest = lowest_points(depths)
+    basin_sizes = [find_basin_size(depths, p) for p in lowest]
+    basin_sizes = sorted(basin_sizes, reverse=True)
+    return basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
+
+
+def find_basin_size(depths, point):
+    basin = { key: False for key in depths.keys() }
+    _, size = dfs(depths, point, basin)
+    return size
+
+
+def dfs(graph, node, tree, size=0):
+    if not tree[node]:
+        tree[node] = True
+        if graph[node] < 9:
+            size += 1
+            neigbours = find_neighbours(node, graph)
+            for n in neigbours:
+                tree, size = dfs(graph, n, tree, size)
+    return tree, size
 
 
 with open('input.txt', 'r') as f:
-    data = [i.rstrip() for i in f]
+    data = [list(map(int, list(i.rstrip()))) for i in f]
 
-print(f"Part one solution: {part_one(data)}")
-# print(f"Part two solution: {part_two(data)}")
+carthesian_product = list(product(range(len(data[0])), range(len(data))))
+depths = { key: data[key[1]][key[0]] for key in carthesian_product }
+
+print(f"Part one solution: {part_one(depths)}")
+print(f"Part two solution: {part_two(depths)}")

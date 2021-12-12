e6a12c84042b7d3e6250f835fe102b874536afb0
spoohie <kplatkowski@outlook.com>
2021-12-12 15:07:49 +0100

    Add solution for day 12

---
+++ aoc12.py
@@ -0,0 +1,64 @@
+from collections import Counter, defaultdict
+
+
+def part_one(paths):
+    return find_paths(paths)
+
+
+def find_paths(paths, node='start', path=[], count=0):
+    if node.islower() and node in path:
+        return 0
+    path.append(node)
+    if node == 'end':
+        return 1
+    for n in paths[node]:
+        if n.islower() and n in path:
+            continue
+        count += find_paths(paths, n, path[:])
+    return count
+
+
+def part_two(paths):
+    return find_paths_two(paths)
+
+
+def find_paths_two(paths, node='start', path=[], count=0):
+    if not can_put_node(node, path):
+        return 0
+    path.append(node)
+    if 'end' in path:
+        return 1
+    for n in paths[node]:
+        if not can_put_node(n, path):
+            continue
+        count += find_paths_two(paths, n, path[:])
+    return count
+
+
+def can_put_node(node, path):
+    if node == 'start' or node == 'end':
+        if node in path:
+            return False
+    if node.islower() and node in path and path_contains_lower_pair(path):
+        return False
+    return True
+
+
+def path_contains_lower_pair(path):
+    c = dict(Counter(path))
+    for k, v in c.items():
+        if k.islower() and v == 2:
+            return True
+    return False
+
+
+with open('input.txt', 'r') as f:
+    data = [tuple(i.strip().split('-')) for i in f]
+adjacency_list = defaultdict(list)
+for src, dest in data:
+    adjacency_list[src].append(dest)
+    adjacency_list[dest].append(src)
+
+
+print(f"Part one solution: {part_one(adjacency_list)}")
+print(f"Part two solution: {part_two(adjacency_list)}")

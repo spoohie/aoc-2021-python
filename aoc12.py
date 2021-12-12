65f27d266f183de2d4322a4c729b092f1c2fc7c1
spoohie <kplatkowski@outlook.com>
2021-12-12 16:46:43 +0100

    Refactor for day 12

--- aoc12.py
+++ aoc12.py
@@ -5,37 +5,28 @@ def part_one(paths):
     return find_paths(paths)
 
 
-def find_paths(paths, node='start', path=[], count=0):
-    if node.islower() and node in path:
+def condition_part_one(node, path):
+    return not (node.islower() and node in path)
+
+
+def find_paths(paths, node='start', path=[], count=0, can_put_node=condition_part_one):
+    if not can_put_node(node, path):
         return 0
     path.append(node)
     if node == 'end':
         return 1
     for n in paths[node]:
-        if n.islower() and n in path:
+        if not can_put_node(n, path):
             continue
-        count += find_paths(paths, n, path[:])
+        count += find_paths(paths, n, path[:], can_put_node=can_put_node)
     return count
 
 
 def part_two(paths):
-    return find_paths_two(paths)
+    return find_paths(paths, path=[], can_put_node=condition_part_two)
 
 
-def find_paths_two(paths, node='start', path=[], count=0):
-    if not can_put_node(node, path):
-        return 0
-    path.append(node)
-    if 'end' in path:
-        return 1
-    for n in paths[node]:
-        if not can_put_node(n, path):
-            continue
-        count += find_paths_two(paths, n, path[:])
-    return count
-
-
-def can_put_node(node, path):
+def condition_part_two(node, path):
     if node == 'start' or node == 'end':
         if node in path:
             return False
